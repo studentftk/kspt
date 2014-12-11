832dca8572c82f7f694b0a77ee329fcf4db3fc60
oglandx <hax_hax@mail.ru>
2014-12-12 01:44:12 +0300

    Исправлены имена

--- Android/StudentFTK/app/src/main/java/com/example/izual/studentftk/FragmentMessages.java
+++ Android/StudentFTK/app/src/main/java/com/example/izual/studentftk/FragmentMessages.java
@@ -5,19 +5,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
  * Created by Антон on 12.12.2014.
  */
 public class FragmentMessages extends Fragment {
+    private ListView listMessages;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         // return super.onCreateView(inflater, container, savedInstanceState);
 
-        View vieMessages = inflater.inflate(R.layout.fragment_messages, container, false);
+        View viewMessages = inflater.inflate(R.layout.fragment_messages, container, false);
 
-        return vieMessages;
+        listMessages = (ListView)viewMessages.findViewById(R.id.listMessages);
+
+        return viewMessages;
     }
+
+    /**
+     * Send message
+     */
+    public void btnSendMessageClick(View view){
+
+    }
+
 }
