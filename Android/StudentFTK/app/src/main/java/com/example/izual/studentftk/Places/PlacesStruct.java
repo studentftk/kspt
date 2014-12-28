b3878b508fd002703cc9d211695499def3ede19f
newmumba <abramov91@mail.ru>
2014-12-29 02:33:31 +0300

    11 hours before

--- Android/StudentFTK/app/src/main/java/com/example/izual/studentftk/Places/PlacesStruct.java
+++ Android/StudentFTK/app/src/main/java/com/example/izual/studentftk/Places/PlacesStruct.java
@@ -7,6 +7,28 @@ import org.json.simple.JSONObject;
  */
 public class PlacesStruct {
 
+    public final String ID;
+    public final static String[] KEYS = {"id","houseCorp","title","geo","street","about","houseNumber","type"};
+    public final static int OBJECTS_COUNT = KEYS.length;
+
     public PlacesStruct(JSONObject jsonObject) {
+        final String[] data = ParseObject(jsonObject);
+        ID = data[0];
+    }
+
+    private String[] ParseObject(JSONObject jsonObject) {
+        String stringObjects[] = new String[PlacesStruct.OBJECTS_COUNT];
+        String keys[] = PlacesStruct.KEYS;
+
+        for(int i = 0; i < PlacesStruct.OBJECTS_COUNT; i++){
+            Object parsedObject = jsonObject.get(keys[i]);
+            if(parsedObject == null){
+                stringObjects[i] = "null";
+            }
+            else {
+                stringObjects[i] = parsedObject.toString();
+            }
+        }
+        return stringObjects;
     }
 }
