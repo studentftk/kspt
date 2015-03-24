9736ebc0355e9bd7d1e423c77728a6c152ed6c26
fdr91 <fdr91@mail.ru>
2015-03-24 04:15:28 +0300

    Markers' initialization on MapsFragment changed. Information about the buildings moved from java code to xml resource res/values/housings.xml

--- Android/StudentFTK/app/src/main/java/com/example/izual/studentftk/FragmentMaps.java
+++ Android/StudentFTK/app/src/main/java/com/example/izual/studentftk/FragmentMaps.java
@@ -1,6 +1,7 @@
 package com.example.izual.studentftk;
 
 import android.app.Fragment;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.InflateException;
 import android.view.LayoutInflater;
@@ -66,6 +67,31 @@ public class FragmentMaps extends Fragment {
         }
     }
 
+    private static String getFieldValue(String entry, String field){
+        int index = entry.indexOf(field)+field.length()+2;
+        int start = index+1;
+        int end = entry.indexOf("\"", start);
+        while(entry.charAt(end-1) == '\\'){
+            start =  end+1;
+            end = entry.indexOf("\"", start);
+        }
+        return entry.substring(index, end);
+    }
+
+    private void setupMarkers(){
+        for(String entry : getResources().getStringArray(R.array.housings)){
+            String[] pa = getFieldValue(entry, "position").split(",");
+            String markerName = getFieldValue(entry, "icon_name");
+            String title = getFieldValue(entry, "title");
+            double position1 = Double.parseDouble(pa[0]);
+            double position2 = Double.parseDouble(pa[1]);
+            mMap.addMarker(new MarkerOptions().position(new LatLng(position1, position2))
+                    .icon(BitmapDescriptorFactory.fromResource(
+                            getResources().getIdentifier(markerName, "drawable", getActivity().getApplicationContext().getPackageName())))
+                    .title(title));
+        }
+    }
+
     private void setUpMap() {
 
         CameraPosition cameraPosition = new CameraPosition.Builder()
@@ -76,30 +102,6 @@ public class FragmentMaps extends Fragment {
                 .build();
         CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
         mMap.animateCamera(cameraUpdate);
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.007387, 30.372935)).icon(BitmapDescriptorFactory.fromResource(R.drawable.gz)).title("Главный учебный корпус СПбПУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.006716, 30.376414)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("Химический корпус СПбПУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.008072, 30.377154)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("Механический корпус СПбПУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.005817, 30.381896)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("Гидрокорпус-1 СПбПУ "));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.006598, 30.383565)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("Гидрокорпус-2 СПбПУ ИСИ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.005479, 30.374064)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("Гидробашня СПбПУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.007566, 30.379906)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("Лабораторный корпус СПбПУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.002972, 30.374054)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("НОЦ РАН СПбПУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.008855, 30.372992)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("1 корпус СПбПУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.008566, 30.374665)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("2 корпус СПбПУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.007273, 30.381703)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("3 корпус СпбПУ ИЭИ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.007281, 30.376811)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("4 корпус СПбПУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(59.999709, 30.374815)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("5 корпус СПбПУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.000145, 30.367498)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("6 корпус СПбПУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(59.999437, 30.372058)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("8 корпус СПбПУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.000823, 30.366485)).icon(BitmapDescriptorFactory.fromResource(R.drawable.ftk)).title("9 корпус СПбПУ ИИТУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.000579, 30.369558)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("10 корпус СПбПУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.009263, 30.378377)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("11 корпус СПбПУ ИИТУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.007316, 30.390281)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("15 корпус СПбПУ ИМОП"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.007676, 30.390796)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("Общежитие №15 СПбПУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.00767, 30.390029)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("16 корпус СПбПУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.005002, 30.369966)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("1 профессорский корпус СПбПУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.004997, 30.378066)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("2 профессорский корпус СПбПУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.00435, 30.379987)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("Дом ученых в Лесном СПбПУ"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(60.002795, 30.368968)).icon(BitmapDescriptorFactory.fromResource(R.drawable.stud)).title("Спортивный комплекс СПбПУ"));
+        setupMarkers();
     }
 }
