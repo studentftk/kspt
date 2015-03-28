26945a3e4ead9779696d48d193713fc141fa16b0
unknown <tanastasiiaa@gmail.com>
2015-03-28 18:56:26 +0300

    change IP adress

---
+++ Server/src/server/api/GetFriendApi.java
@@ -0,0 +1,19 @@
+package server.api;
+
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import server.core.ApiMethod;
+import server.core.HttpCode;
+import server.entity.User;
+import server.io.JSONHelper;
+import server.logic.FriendDAO;
+import server.logic.UserDAO;
+public class GetFriendApi implements ApiMethod{
+    
+     @Override
+    public ApiAnswer execute(Map<String, String> params) {
+        
+    }
+    
+}
