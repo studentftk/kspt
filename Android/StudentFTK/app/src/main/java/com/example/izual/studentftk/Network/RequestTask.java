0cc8c14aa6f5ba240339b5ff5cd976cce1d95ce4
oglandx <hax_hax@mail.ru>
2015-04-28 22:22:36 +0300

    Fixed security on client
    Just for messages (get/send), user.get, users.get and places.get. oauth
    still work invariably

--- Android/StudentFTK/app/src/main/java/com/example/izual/studentftk/Network/RequestTask.java
+++ Android/StudentFTK/app/src/main/java/com/example/izual/studentftk/Network/RequestTask.java
@@ -2,6 +2,7 @@ package com.example.izual.studentftk.Network;
 
 import android.app.Activity;
 
+import com.example.izual.studentftk.Exceptions.ExceptionsParser;
 import com.example.izual.studentftk.Utils;
 
 import org.json.simple.JSONArray;
@@ -67,6 +68,7 @@ public class RequestTask implements Runnable {
             InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "utf-8");
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
             data = bufferedReader.readLine();
+            ExceptionsParser.TryToRaise(data);
             dataReady = true;
         }
         catch(Exception e){
